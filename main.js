7397806280b8e7cf750ba9804d9bba57c526b4df
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-05-24 12:29:28 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,49 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
+app.disableHardwareAcceleration()
+
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 700,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
       contextIsolation: false,
+      enableRemoteModule: true,
+      backgroundThrottling: false
     },
     autoHideMenuBar: true,
   })
-
+  require('@electron/remote/main').initialize()
+  require('@electron/remote/main').enable(mainWindow.webContents)
+  process.env.window = mainWindow.id
+  mainWindow.on('show', () => {
+    setTimeout(() => {
+      mainWindow.focus()
+    }, 200)
+  })
   mainWindow.loadFile('index.html')
+  if (process.platform === 'win32') {
+    app.setAppUserModelId("TREM | 台灣實時地震監測")
+  }
+  mainWindow.on('close', (event) => {
+    if (app.quitting) {
+      mainWindow = null
+    } else {
+      event.preventDefault()
+      mainWindow.hide()
+    }
+  })
 }
 
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+app.whenReady().then(() => { createWindow() })
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
+
+app.on('activate', () => { win.show() })
+
+app.on('before-quit', () => app.quitting = true)
