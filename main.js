cd2a94f7fd3528a9b0e3ca4b72a7a16940626ebe
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-05-25 03:25:22 +0800

    Add files via upload (#15)

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Tray, Menu, nativeImage } = require('electron')
+const windowStateKeeper = require('electron-window-state');
 const path = require('path')
 
+process.env.Version = "1.6"
+
 let mainWindow = null
-process.env.Version = "1.5"
+let tray = null
 
 app.disableHardwareAcceleration()
 
@@ -13,9 +16,16 @@ app.setLoginItemSettings({
 })
 
 function createWindow() {
+  let mainWindowStateKeeper = windowStateKeeper({
+    defaultWidth: 1000,
+    defaultHeight: 800
+  });
   mainWindow = new BrowserWindow({
-    width: 1024,
-    height: 768,
+    title: 'main',
+    x: mainWindowStateKeeper.x,
+    y: mainWindowStateKeeper.y,
+    width: mainWindowStateKeeper.width,
+    height: mainWindowStateKeeper.height,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
@@ -25,6 +35,7 @@ function createWindow() {
     },
     autoHideMenuBar: true,
   })
+  mainWindowStateKeeper.manage(mainWindow)
   require('@electron/remote/main').initialize()
   require('@electron/remote/main').enable(mainWindow.webContents)
   process.env.window = mainWindow.id
@@ -49,7 +60,43 @@ if (!shouldQuit) {
     mainWindow.restore()
     mainWindow.show()
   })
-  app.whenReady().then(() => { createWindow() })
+  app.whenReady().then(() => {
+    const iconPath = path.join(__dirname, 'TREM.ico')
+    tray = new Tray(nativeImage.createFromPath(iconPath))
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: '開啟',
+        type: 'normal',
+        click: () => {
+          mainWindow.restore()
+          mainWindow.show()
+        }
+      },
+      {
+        label: '隱藏',
+        type: 'normal',
+        click: () => {
+          mainWindow.hide()
+        }
+      },
+      {
+        label: '強制關閉',
+        type: 'normal',
+        click: () => app.quit()
+      }
+    ])
+    tray.setToolTip('TREM | 台灣實時地震監測')
+    tray.setContextMenu(contextMenu)
+    tray.setIgnoreDoubleClickEvents(true)
+    tray.on('click', function (e) {
+      if (mainWindow.isVisible()) {
+        mainWindow.hide()
+      } else {
+        mainWindow.show()
+      }
+    })
+    createWindow()
+  })
 }
 
 app.on('window-all-closed', function () {
