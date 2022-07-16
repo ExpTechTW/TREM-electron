1ae9150a955c0085af15b2f62e5bf4ef533167fa
whes1015 <whes1015@gmail.com>
2022-07-16 15:30:28 +0800

    build: 4.0.0-pre.4

--- TREM/js/earthquake.js
+++ TREM/js/earthquake.js
@@ -167,9 +167,8 @@ function init() {
 			ReportMarkID = null;
 			for (let index = 0; index < MarkList.length; index++)
 				map.removeLayer(MarkList[index]);
-
-			focus();
 		}
+		focus();
 	});
 
 	mapLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw", {
@@ -281,7 +280,7 @@ function init() {
 							document.getElementById("rt-station-pga").innerText = amount;
 						}
 						map.addLayer(ReportMark);
-						ReportMark.setZIndexOffset(1000);
+						ReportMark.setZIndexOffset(2000 + amount);
 						Station[Object.keys(Json)[index]] = ReportMark;
 						if (pga[station[Object.keys(Json)[index]].PGA] == undefined && Intensity != "NA")
 							pga[station[Object.keys(Json)[index]].PGA] = {
@@ -338,6 +337,7 @@ function init() {
 							let town = list[1];
 							let ReportMark = L.marker([Location[city][town][1], Location[city][town][2]], { icon: myIcon });
 							map.addLayer(ReportMark);
+							ReportMark.setZIndexOffset(1500 + PAlert.data[index].intensity);
 							Station[PAlert.data[index].loc] = ReportMark;
 							if (PAlert.data[index].intensity > pga[PAlert.data[index].TREM].Intensity) pga[PAlert.data[index].TREM].Intensity = PAlert.data[index].intensity;
 							pga[PAlert.data[index].TREM].Time = NOW.getTime();
@@ -363,10 +363,13 @@ function init() {
 							delete pga[Object.keys(pga)[index]];
 							index--;
 						} else {
-							PGA[Object.keys(pga)[index]] = L.polygon(PGAjson[Object.keys(pga)[index].toString()], {
+							let Pmarker = L.polygon(PGAjson[Object.keys(pga)[index].toString()], {
 								color     : color(Intensity),
 								fillColor : "transparent",
-							}).addTo(map);
+							});
+							map.addLayer(Pmarker);
+							Pmarker.setZIndexOffset(1000 + Intensity);
+							PGA[Object.keys(pga)[index]] = Pmarker;
 							PGAaudio = true;
 						}
 					}
