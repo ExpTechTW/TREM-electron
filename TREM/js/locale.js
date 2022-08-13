113fc864545be14cb894bf6f688af38bd29cadc6
kamiya10 <58339640+kamiya10@users.noreply.github.com>
2022-08-13 12:34:11 +0800

    feat: russian localization

--- TREM/js/locale.js
+++ TREM/js/locale.js
@@ -1,5 +1,5 @@
 const setLocale = (language) => {
-	if (["en", "ja", "zh-TW"].includes(language)) {
+	if (["en", "ja", "ru", "zh-TW"].includes(language)) {
 		const lang = ":lang(" + language + ")";
 		const hide = ":not(html)[lang]:not(" + lang + ")";
 		document.querySelectorAll(hide).forEach((node) => node.style.display = "none");
