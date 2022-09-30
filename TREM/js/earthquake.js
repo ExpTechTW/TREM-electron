2def8e0b90ed260f04aafdb8cd49a8d24e5d2df2
NYJ36 <yangheahyun5425@gmail.com>
2022-09-30 16:01:19 +0900

    클래스 변경

--- TREM/js/earthquake.js
+++ TREM/js/earthquake.js
@@ -1044,27 +1044,8 @@ function addReport(report, prepend = false) {
 		const report_intensity_container = document.createElement("div");
 		report_intensity_container.className = "report-intensity-container";
 
-		const report_intenisty_title_en = document.createElement("span");
-		report_intenisty_title_en.lang = "en";
-		report_intenisty_title_en.className = "report-intenisty-title";
-		report_intenisty_title_en.innerText = "Max Int.";
-		const report_intenisty_title_ja = document.createElement("span");
-		report_intenisty_title_ja.lang = "ja";
-		report_intenisty_title_ja.className = "report-intenisty-title";
-		report_intenisty_title_ja.innerText = "最大震度";
-		const report_intenisty_title_kr = document.createElement("span");
-		report_intenisty_title_kr.lang = "kr";
-		report_intenisty_title_kr.className = "report-intenisty-title";
-		report_intenisty_title_kr.innerText = "최대진도";
-		const report_intenisty_title_ru = document.createElement("span");
-		report_intenisty_title_ru.lang = "ru";
-		report_intenisty_title_ru.className = "report-intenisty-title";
-		report_intenisty_title_ru.innerText = "Макс интенси";
-		report_intenisty_title_ru.style = "font-size: 14px;line-height: 14px";
-		const report_intenisty_title_zh_tw = document.createElement("span");
-		report_intenisty_title_zh_tw.lang = "zh-TW";
-		report_intenisty_title_zh_tw.className = "report-intenisty-title";
-		report_intenisty_title_zh_tw.innerText = "最大震度";
+		const report_intensity_title_container = document.createElement("div");
+		report_intensity_title_container.className = "report-intensity-title-container";
 
 		const report_intensity_title_en = document.createElement("span");
 		report_intensity_title_en.lang = "en";
@@ -1117,11 +1098,11 @@ function addReport(report, prepend = false) {
 		const report_container = document.createElement("div");
 		report_container.className = "report-container";
 
-		const report_intenisty_container = document.createElement("div");
-		report_intenisty_container.className = "report-intenisty-container";
+		const report_intensity_container = document.createElement("div");
+		report_intensity_container.className = "report-intensity-container";
 
-		const report_intenisty_title_container = document.createElement("div");
-		report_intenisty_title_container.className = "report-intenisty-title-container";
+		const report_intensity_title_container = document.createElement("div");
+		report_intensity_title_container.className = "report-intensity-title-container";
 
 		const report_intensity_title_en = document.createElement("span");
 		report_intensity_title_en.lang = "en";
@@ -1148,10 +1129,10 @@ function addReport(report, prepend = false) {
 		report_intensity_title_container.append(report_intensity_title_en, report_intensity_title_ja, report_intensity_title_kr, report_intensity_title_ru, report_intensity_title_zh_tw);
 		report_intensity_title_container.childNodes.forEach((node) => node.style.display = node.lang == setting["general.locale"] ? "unset" : "none");
 
-		const report_intenisty_value = document.createElement("span");
-		report_intenisty_value.className = "report-intenisty-value";
-		report_intenisty_value.innerText = Level;
-		report_intenisty_container.append(report_intenisty_title_container, report_intenisty_value);
+		const report_intensity_value = document.createElement("span");
+		report_intensity_value.className = "report-intensity-value";
+		report_intensity_value.innerText = Level;
+		report_intensity_container.append(report_intensity_title_container, report_intensity_value);
 
 
 		const report_detail_container = document.createElement("div");
