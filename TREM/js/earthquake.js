2b608680d5a30ad7f482025e0a1a6c71b176bfbe
whes1015 <whes1015@gmail.com>
2022-10-03 22:50:06 +0800

    fix: 震度列表

--- TREM/js/earthquake.js
+++ TREM/js/earthquake.js
@@ -700,42 +700,42 @@ async function handler(response) {
 		win.setAlwaysOnTop(false);
 		PGAtag = All[0].intensity;
 	}
-	const list = [];
+	let list = [];
 	let count = 0;
-	if (All.length >= 2)
-		if (All.length <= 8)
-			for (let Index = 0; Index < All.length; Index++, count++) {
-				if (count >= 8) break;
-				const container = document.createElement("DIV");
-				container.className = IntensityToClassString(All[Index].intensity);
-				const location = document.createElement("span");
-				location.innerText = `${All[Index].loc}\n${All[Index].pga} gal`;
-				container.appendChild(document.createElement("span"));
-				container.appendChild(location);
-				list.push(container);
-			}
-		else {
-			const Idata = {};
-			for (let Index = 0; Index < All.length; Index++, count++) {
-				if (Object.keys(Idata).length >= 8) break;
-				const city = All[Index].loc.split(" ")[0];
-				const CPGA = (Idata[city] == undefined) ? 0 : Idata[city];
-				if (All[Index].pga > CPGA) {
-					if (Idata[city] == undefined)Idata[city] = {};
-					Idata[city].pga = All[Index].pga;
-					Idata[city].intensity = All[Index].intensity;
-				}
-			}
-			for (let index = 0; index < Object.keys(Idata).length; index++) {
-				const container = document.createElement("DIV");
-				container.className = IntensityToClassString(Idata[Object.keys(Idata)[index]].intensity);
-				const location = document.createElement("span");
-				location.innerText = `${Object.keys(Idata)[index]}\n${Idata[Object.keys(Idata)[index]].pga} gal`;
-				container.appendChild(document.createElement("span"));
-				container.appendChild(location);
-				list.push(container);
+	if (All.length <= 8)
+		for (let Index = 0; Index < All.length; Index++, count++) {
+			if (count >= 8) break;
+			const container = document.createElement("DIV");
+			container.className = IntensityToClassString(All[Index].intensity);
+			const location = document.createElement("span");
+			location.innerText = `${All[Index].loc}\n${All[Index].pga} gal`;
+			container.appendChild(document.createElement("span"));
+			container.appendChild(location);
+			list.push(container);
+		}
+	else {
+		const Idata = {};
+		for (let Index = 0; Index < All.length; Index++, count++) {
+			if (Object.keys(Idata).length >= 8) break;
+			const city = All[Index].loc.split(" ")[0];
+			const CPGA = (Idata[city] == undefined) ? 0 : Idata[city];
+			if (All[Index].pga > CPGA) {
+				if (Idata[city] == undefined)Idata[city] = {};
+				Idata[city].pga = All[Index].pga;
+				Idata[city].intensity = All[Index].intensity;
 			}
 		}
+		for (let index = 0; index < Object.keys(Idata).length; index++) {
+			const container = document.createElement("DIV");
+			container.className = IntensityToClassString(Idata[Object.keys(Idata)[index]].intensity);
+			const location = document.createElement("span");
+			location.innerText = `${Object.keys(Idata)[index]}\n${Idata[Object.keys(Idata)[index]].pga} gal`;
+			container.appendChild(document.createElement("span"));
+			container.appendChild(location);
+			list.push(container);
+		}
+	}
+	if (!Json.Alert) list = [];
 	document.getElementById("rt-list").replaceChildren(...list);
 }
 
