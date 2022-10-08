e91738a99e22f05f4c2abf87eb4b6382f96bf521
kamiya10 <58339640+kamiya10@users.noreply.github.com>
2022-10-09 02:42:24 +0800

    feat: filters

--- TREM/js/report.js
+++ TREM/js/report.js
@@ -1,30 +1,40 @@
 /* global IntensityToClassString: false, reportCache: false */
 TREM.Report = {
-	/**
-     * @type {HTMLTemplateElement}
-     */
-	_reportItemTemplate: document.getElementById("template-report-list-item"),
+	view                  : "list",
+	_filterHasReplay      : false,
+	_filterHasNumber      : false,
+	_filterMagnitude      : false,
+	_filterMagnitudeValue : false,
+	_filterIntensity      : false,
+	_filterIntensityValue : false,
+	_reportItemTemplate   : document.getElementById("template-report-list-item"),
 	unloadReports() {
 		document.getElementById("report-list-container").replaceChildren();
 	},
 	loadReports() {
 		const fragment = new DocumentFragment();
+		const reports = reportCache
+			.filter(v => this._filterHasNumber ? v.earthquakeNo % 1000 != 0 : true)
+			.filter(v => this._filterHasReplay ? v.ID?.length : true)
+			.filter(v => this._filterMagnitude ? this._filterMagnitudeValue == 1 ? v.magnitudeValue < 4 : v.magnitudeValue >= 4 : true)
+			.filter(v => this._filterIntensity ? v.data[0].areaIntensity == this._filterIntensityValue : true);
 
-		for (const report of reportCache)
+		for (const report of reports)
 			fragment.appendChild(this._createReportItem(report));
 
-		console.log(fragment);
 		document.getElementById("report-list-container").appendChild(fragment);
 	},
-	/**
-     * @param {EarthquakeReport} data
-     */
 	_createReportItem(data) {
 		const el = this._reportItemTemplate.content.cloneNode(true);
-		el.querySelector(".report-list-item-intensity").className += ` ${IntensityToClassString(data.data[0].areaIntensity)}`;
+		el.querySelector(".report-list-item").className += ` ${IntensityToClassString(data.data[0].areaIntensity)}`;
 		el.querySelector(".report-list-item-location").innerText = data.location;
 		el.querySelector(".report-list-item-time").innerText = data.originTime.replace(/-/g, "/");
 		return el;
+	},
+	_handleFilter(key, value) {
+		this[`_${key}`] = value;
+		this.unloadReports();
+		this.loadReports();
 	},
 };
 
