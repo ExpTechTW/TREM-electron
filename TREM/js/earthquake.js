ec55c0c616dc9fb6244826e8c64152382df43377
夜夜 <vcde0000@gmail.com>
2022-10-10 22:13:56 +0800

    5.1.4-32-pre4
    https://github.com/ExpTechTW/TREM/commit/faf5b57e5f1a0f5a75a0f5d6f93fb573c8fa577a

--- TREM/js/earthquake.js
+++ TREM/js/earthquake.js
@@ -379,7 +379,7 @@ async function init() {
 				style     : {
 					weight      : 0.8,
 					color       : colors.primary,
-					fillColor   : "transparent",
+					fillColor   : colors.surfaceVariant,
 					fillOpacity : 0,
 				},
 			}).addTo(mapReport);
