62f79547c8ab69d9263a0640850595788aa47f68
夜夜 <vcde0000@gmail.com>
2022-10-11 23:09:53 +0800

    5.1.4-54-pre6
    https://github.com/ExpTechTW/TREM/commit/70bd112bfa00d42ef8349308f9f528c206172754
    https://github.com/ExpTechTW/TREM/commit/3b97ae75069fb617de9c74c526ab4cba9150e583
    https://github.com/ExpTechTW/TREM/commit/c7f9f97d498e6879a9b61e41f03b158ab80b7d38
    https://github.com/ExpTechTW/TREM/commit/740addb1ffa2da68820e29a24e6628a641589d09
    https://github.com/ExpTechTW/TREM/commit/74d5f42d534ae2e50cc0e77cb37a7d9d678adb35

--- TREM/js/locale.js
+++ TREM/js/locale.js
@@ -1,5 +1,5 @@
 const setLocale = (language) => {
-	if (["en", "ja", "kr", "ru", "zh-TW"].includes(language)) {
+	if (["en", "ja", "kr", "ru", "zh-CN", "zh-TW"].includes(language)) {
 		const lang = ":lang(" + language + ")";
 		const hide = ":not(html)[lang]:not(" + lang + ")";
 		document.querySelectorAll(hide).forEach((node) => node.style.display = "none");
