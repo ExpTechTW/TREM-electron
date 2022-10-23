1bda4a5948b0d322e73990fa81ff1f96e97f0bef
kamiya10 <58339640+kamiya10@users.noreply.github.com>
2022-10-23 13:52:28 +0800

    chore: namespace change

---
+++ TREM.Electron/Localization/Localization.js
@@ -0,0 +1,34 @@
+const Dictionary = require("./Dictionary.js");
+
+class Localization {
+	constructor(defaultLocale, fallbackLocale = "zh-TW") {
+		this.defaultLocale = defaultLocale;
+		this.fallbackLocale = Localization.availableLocales.includes(fallbackLocale) ? fallbackLocale
+			: Localization.availableLocales.includes(fallbackLocale.slice(0, 2)) ? fallbackLocale.slice(0, 2)
+				: fallbackLocale.startsWith("zh") ? "zh-TW"
+					: "en";
+
+		for (const locale of Localization.availableLocales)
+			this[locale] = new Dictionary(locale);
+	}
+
+	static availableLocales = ["en", "ja", "kr", "ru", "zh-CN", "zh-TW"];
+
+	matchLocale(value) {
+		value = Localization.availableLocales.includes(value) ? value
+			: Localization.availableLocales.includes(value.slice(0, 2)) ? value.slice(0, 2)
+				: value.startsWith("zh") ? "zh-TW"
+					: "en";
+		return value;
+	}
+
+	setLocale(locale) {
+		this.defaultLocale = locale;
+	}
+
+	getString(id, locale) {
+		return this[locale]?.get(id) ?? this[this.defaultLocale]?.get(id) ?? this[this.fallbackLocale]?.get(id);
+	}
+}
+
+module.exports = Localization;
---
+++ TREM.Electron/js/locale.js
@@ -0,0 +1,10 @@
+const setLocale = (language) => {
+	if (["en", "ja", "kr", "ru", "zh-CN", "zh-TW"].includes(language)) {
+		const lang = ":lang(" + language + ")";
+		const hide = ":not(html)[lang]:not(" + lang + ")";
+		document.querySelectorAll(hide).forEach((node) => node.style.display = "none");
+		const show = "[lang]" + lang;
+		document.querySelectorAll(show).forEach((node) => node.style.display = "unset");
+		document.documentElement.lang = language;
+	}
+};
