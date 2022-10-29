d0c5d48ad44e8024d9c281cef39837f0493e752e
夜夜 <vcde0000@gmail.com>
2022-10-30 02:05:29 +0800

    6.0.0-23-pre3
    https://github.com/ExpTechTW/TREM/commit/d56b24d3acfcd5185ccd0dc9fdff9e76f7d4e7cf

--- TREM.Electron/js/earthquake.js
+++ TREM.Electron/js/earthquake.js
@@ -376,18 +376,22 @@ async function init() {
 					center            : [121.596, 23.612],
 					renderWorldCopies : false,
 					keyboard          : false,
+					doubleClickZoom   : false,
 				})
-				.on("click", () => Maps.main.flyTo({
-					center   : [121.596, 23.612],
-					zoom     : 6.8,
-					bearing  : 0,
-					speed    : 2,
-					curve    : 1,
-					easing   : (e) => Math.sin(e * Math.PI / 2),
-					duration : 1000,
-				}))
+				.on("click", (ev) => {
+					if (ev.originalEvent.target.tagName == "CANVAS")
+						Maps.main.flyTo({
+							center   : [121.596, 23.612],
+							zoom     : 6.8,
+							bearing  : 0,
+							speed    : 2,
+							curve    : 1,
+							easing   : (e) => Math.sin(e * Math.PI / 2),
+							duration : 1000,
+						});
+				})
 				.on("zoom", () => {
-					if (Maps.main.getZoom() >= 12) {
+					if (Maps.main.getZoom() >= 11.5) {
 						for (const key in Station)
 							if (!Station[key].getPopup().isOpen())
 								Station[key].togglePopup();
@@ -396,15 +400,18 @@ async function init() {
 							if (!Station[key].getPopup().persist)
 								Station[key].togglePopup();
 				})
-				.on("contextmenu", () => Maps.main.flyTo({
-					center   : [121.596, 23.612],
-					zoom     : 6.8,
-					bearing  : 0,
-					speed    : 2,
-					curve    : 1,
-					easing   : (e) => Math.sin(e * Math.PI / 2),
-					duration : 1000,
-				}));
+				.on("contextmenu", (ev) => {
+					if (ev.originalEvent.target.tagName == "CANVAS")
+						Maps.main.flyTo({
+							center   : [121.596, 23.612],
+							zoom     : 6.8,
+							bearing  : 0,
+							speed    : 2,
+							curve    : 1,
+							easing   : (e) => Math.sin(e * Math.PI / 2),
+							duration : 1000,
+						});
+				});
 
 		if (!Maps.mini)
 			Maps.mini = L.map("map-tw",
@@ -769,60 +776,8 @@ function handler(response) {
 							(amount > 2) ? "pga2" :
 								"pga1";
 
-		const station_tooltip = `<div>${station[keys[index]].Loc}</div><div>${amount}</div><div>${IntensityI(Intensity)}</div>`;
-		// const station_tooltip = `<div class="rt-station-tooltip">${station[keys[index]].Loc}</div><div>${amount}</div><div>${IntensityI(Intensity)}</div>`;
-		/*
-
-		if (!Station[keys[index]])
-			Station[keys[index]] = L.marker(
-				[station[keys[index]].Lat, station[keys[index]].Long],
-				{
-					icon: L.divIcon({
-						iconSize  : [size, size],
-						className : `map-intensity-icon rt-icon ${levelClass}`,
-					}),
-					keyboard: false,
-				})
-				.addTo(Maps.main)
-				.bindTooltip(station_tooltip, {
-					offset    : [8, 0],
-					permanent : false,
-					className : "rt-station-tooltip",
-				})
-				.on("click", () => {
-					// Station[keys[index]].keepTooltipAlive = !Station[keys[index]].keepTooltipAlive;
-					// if (Maps.main.getZoom() < 11) {
-					// 	const tooltip = Station[keys[index]].getTooltip();
-					// 	Station[keys[index]].unbindTooltip();
-					// 	if (Station[keys[index]].keepTooltipAlive)
-					// 		tooltip.options.permanent = true;
-					// 	else
-					// 		tooltip.options.permanent = false;
-					// 	Station[keys[index]].bindTooltip(tooltip);
-					// }
-					if (rtstation1 == "")
-						rtstation1 = keys[index];
-					else if (rtstation1 == keys[index])
-						rtstation1 = "";
-					else if (rtstation1 != keys[index])
-						rtstation1 = keys[index];
-				})
-				.on("mouseover", () => {
-					const tooltip = Station[keys[index]].getTooltip();
-					tooltip.options.permanent = true;
-				});
-
-		if (Station[keys[index]].getIcon()?.options?.className != `map-intensity-icon rt-icon ${levelClass}`)
-			Station[keys[index]].setIcon(L.divIcon({
-				iconSize  : [size, size],
-				className : `map-intensity-icon rt-icon ${levelClass}`,
-			}));
-
-		Station[keys[index]]
-			.setZIndexOffset(2000 + ~~(amount * 10) + Intensity * 500)
-			.setTooltipContent(station_tooltip);
-
-		*/
+		// const station_tooltip = `<div>${station[keys[index]].Loc}</div><div>${amount}</div><div>${IntensityI(Intensity)}</div>`;
+		const station_tooltip = `<div class="rt-station-tooltip"><div class="rt-station-name">${station[keys[index]].Loc}</div><div class="rt-station-pga">${amount}</div><div>${IntensityI(Intensity)}</div></div>`;
 
 		const station_tooltip_popup = new maplibregl.Popup({ closeOnClick: false, closeButton: false });
 		if (!Station[keys[index]]) {
@@ -833,14 +788,8 @@ function handler(response) {
 				.setLngLat([station[keys[index]].Long, station[keys[index]].Lat])
 				.setPopup(station_tooltip_popup.setHTML(station_tooltip))
 				.addTo(Maps.main);
-			station_tooltip_popup.on("open", () => {
-				if (rtstation1 == "")
-					rtstation1 = keys[index];
-				else if (rtstation1 == keys[index])
-					rtstation1 = "";
-				else if (rtstation1 != keys[index])
-					rtstation1 = keys[index];
-			}).on("close", () => {
+			Station[keys[index]].getElement().addEventListener("click", () => {
+				Station[keys[index]].getPopup().persist = !Station[keys[index]].getPopup().persist
 				if (rtstation1 == "")
 					rtstation1 = keys[index];
 				else if (rtstation1 == keys[index])
@@ -848,6 +797,24 @@ function handler(response) {
 				else if (rtstation1 != keys[index])
 					rtstation1 = keys[index];
 			});
+			Station[keys[index]].getElement().addEventListener("mouseover", () => {
+				Station[keys[index]].getPopup().persist = !Station[keys[index]].getPopup().persist
+			});
+			// station_tooltip_popup.on("open", () => {
+			// 	if (rtstation1 == "")
+			// 		rtstation1 = keys[index];
+			// 	else if (rtstation1 == keys[index])
+			// 		rtstation1 = "";
+			// 	else if (rtstation1 != keys[index])
+			// 		rtstation1 = keys[index];
+			// }).on("close", () => {
+			// 	if (rtstation1 == "")
+			// 		rtstation1 = keys[index];
+			// 	else if (rtstation1 == keys[index])
+			// 		rtstation1 = "";
+			// 	else if (rtstation1 != keys[index])
+			// 		rtstation1 = keys[index];
+			// });
 		} else Station[keys[index]].getPopup().setHTML(station_tooltip);
 
 		if (Station[keys[index]].getElement().className != `map-intensity-icon rt-icon ${levelClass}`)
@@ -858,7 +825,7 @@ function handler(response) {
 
 		if (Unlock) {
 			if (rtstation1 == "") {
-				if (keys.includes(setting["Real-time.station"]))
+				if (keys.includes(setting["Real-time.station"])) {
 					if (keys[index] == setting["Real-time.station"]) {
 						if (document.getElementById("rt-station").classList.contains("hide"))
 							document.getElementById("rt-station").classList.remove("hide");
@@ -868,6 +835,13 @@ function handler(response) {
 						document.getElementById("rt-station-local-time").innerText = now.format("HH:mm:ss");
 						document.getElementById("rt-station-local-pga").innerText = amount;
 					}
+				} else {
+					document.getElementById("rt-station-local-intensity").className = "rt-station-intensity na";
+					document.getElementById("rt-station-local-id").innerText = TREM.Localization.getString("Realtime_No_Data");
+					document.getElementById("rt-station-local-name").innerText = TREM.Localization.getString("Realtime_No_Data");
+					document.getElementById("rt-station-local-time").innerText = "--:--:--";
+					document.getElementById("rt-station-local-pga").innerText = "--";
+				}
 			} else if (rtstation1 == keys[index]) {
 				document.getElementById("rt-station-local-intensity").className = `rt-station-intensity ${(amount < 999 && Intensity != "NA") ? IntensityToClassString(Intensity) : "na"}`;
 				document.getElementById("rt-station-local-id").innerText = keys[index];
@@ -876,7 +850,7 @@ function handler(response) {
 				document.getElementById("rt-station-local-pga").innerText = amount;
 			}
 		} else if (rtstation1 == "") {
-			if (keys.includes(setting["Real-time.station"]))
+			if (keys.includes(setting["Real-time.station"])) {
 				if (keys[index] == setting["Real-time.station"]) {
 					if (document.getElementById("rt-station").classList.contains("hide"))
 						document.getElementById("rt-station").classList.remove("hide");
@@ -886,6 +860,13 @@ function handler(response) {
 					document.getElementById("rt-station-local-time").innerText = now.format("HH:mm:ss");
 					document.getElementById("rt-station-local-pga").innerText = amount;
 				}
+			} else {
+				document.getElementById("rt-station-local-intensity").className = "rt-station-intensity na";
+				document.getElementById("rt-station-local-id").innerText = TREM.Localization.getString("Realtime_No_Data");
+				document.getElementById("rt-station-local-name").innerText = TREM.Localization.getString("Realtime_No_Data");
+				document.getElementById("rt-station-local-time").innerText = "--:--:--";
+				document.getElementById("rt-station-local-pga").innerText = "--";
+			}
 		} else if (rtstation1 == keys[index]) {
 			document.getElementById("rt-station-local-intensity").className = `rt-station-intensity ${(amount < 999 && Intensity != "NA") ? IntensityToClassString(Intensity) : "na"}`;
 			document.getElementById("rt-station-local-id").innerText = keys[index];
