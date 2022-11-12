92bc7299a606b6a658aab1645a676507d7d15b70
whes1015 <whes1015@gmail.com>
2022-11-12 23:09:56 +0800

    fix: GeoJson memory leak

--- TREM.Electron/js/earthquake.js
+++ TREM.Electron/js/earthquake.js
@@ -1545,7 +1545,10 @@ TREM.Earthquake.on("eew", (data) => {
 			if (Intensity > MaxIntensity) MaxIntensity = Intensity;
 			GC[loc[0]] = Intensity;
 		}
-
+	if (EarthquakeList[data.ID].geojson != undefined) {
+		EarthquakeList[data.ID].geojson.remove();
+		delete EarthquakeList[data.ID].geojson;
+	}
 	EarthquakeList[data.ID].geojson = L.geoJson.vt(MapData.tw_town, {
 		minZoom   : 7,
 		maxZoom   : 7,
@@ -2201,7 +2204,6 @@ function main(data) {
 		// remove epicenter cross icons
 		EarthquakeList[data.ID].epicenterIcon.remove();
 		EarthquakeList[data.ID].epicenterIconTW.remove();
-		EarthquakeList[data.ID].geojson.remove();
 		if (EarthquakeList[data.ID].Depth != null) Maps.main.removeLayer(EarthquakeList[data.ID].Depth);
 
 		for (let index = 0; index < INFO.length; index++)
