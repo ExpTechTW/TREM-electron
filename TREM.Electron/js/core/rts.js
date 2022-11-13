1f022d8118545083e1bdb5ca0850e9cca80efb18
whes1015 <whes1015@gmail.com>
2022-11-13 16:59:54 +0800

    feat: worker

---
+++ TREM.Electron/js/core/rts.js
@@ -0,0 +1,15 @@
+const fetch = require("node-fetch");
+
+module.exports = async function([ReplayTime, key], cb) {
+	const controller = new AbortController();
+	setTimeout(() => {
+		controller.abort();
+	}, 1500);
+	let ans = await fetch(`https://exptech.com.tw/api/v1/trem/RTS?time=${ReplayTime}&key=${key}`, { signal: controller.signal }).catch((err) => void 0);
+	if (controller.signal.aborted || ans == undefined) {
+		cb(true);
+		return;
+	}
+	ans = await ans.json();
+	cb(false, ans);
+};
