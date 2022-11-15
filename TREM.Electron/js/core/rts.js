b70017f72e0ef007957d4dd1de75fea511ebc352
whes1015 <whes1015@gmail.com>
2022-11-16 01:37:23 +0800

    fix: server & rts

--- TREM.Electron/js/core/rts.js
+++ TREM.Electron/js/core/rts.js
@@ -4,7 +4,7 @@ module.exports = async function([ReplayTime, key], cb) {
 	const controller = new AbortController();
 	setTimeout(() => {
 		controller.abort();
-	}, 1500);
+	}, 950);
 	let ans = await fetch(`https://exptech.com.tw/api/v1/trem/RTS?time=${ReplayTime}&key=${key}`, { signal: controller.signal }).catch((err) => void 0);
 	if (controller.signal.aborted || ans == undefined) {
 		cb(true);
