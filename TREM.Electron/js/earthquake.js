146f790703d151cc982744728f68785aceac5357
夜夜 <vcde0000@gmail.com>
2022-11-19 00:42:12 +0800

    6.0.0-107-pre9
    https://github.com/ExpTechTW/TREM/commit/acc8be6186723f448925d82048c29127b6a0ee7f
    https://github.com/ExpTechTW/TREM/commit/0bf0c4f32a5e8a3ffc80b96eabd7da286186ce66

--- TREM.Electron/js/earthquake.js
+++ TREM.Electron/js/earthquake.js
@@ -877,7 +877,7 @@ async function init() {
 		if (TREM.Detector.webgl)
 			TREM.Detector.webgl = setting["compatibility.hwaccel"];
 		// TREM.Detector.webgl = false;
-		dump({ level: 0, message: "WebGL: " + TREM.Detector.webgl, origin: "WebGL" });
+		dump({ level: 0, message: TREM.Detector.webgl, origin: "WebGL" });
 
 		if (TREM.Detector.webgl) {
 
